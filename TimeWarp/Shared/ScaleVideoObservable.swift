041dddb929cea78641faed719e36fc40ea8881d9
“jpagliaro” <jpagliaro@me.com>
2022-05-23 20:28:49 -0400

    Changed conditions involving optionals

--- TimeWarp/Shared/ScaleVideoObservable.swift
+++ TimeWarp/Shared/ScaleVideoObservable.swift
@@ -417,7 +417,7 @@ class ScaleVideoObservable:ObservableObject {
                     self.progress = 0
                     self.isScaling = false
                     
-                    if let resultURL = resultURL, self.scaleVideo?.isCancelled == false, self.scaleVideo?.outOfOrder == false {
+                    if let resultURL = resultURL, let scaleVideo = self.scaleVideo, scaleVideo.isCancelled == false, scaleVideo.outOfOrder == false {
                         self.scaledVideoURL = resultURL
                         
                         if let scalingLUT = self.scaleVideo?.scalingLUT {
@@ -429,10 +429,10 @@ class ScaleVideoObservable:ObservableObject {
                         self.playScaled()
                     }
                     else {
-                        if self.scaleVideo?.isCancelled == true {
+                        if let scaleVideo = self.scaleVideo, scaleVideo.isCancelled {
                             self.alertInfo = AlertInfo(id: .scalingFailed, title: "Scaling Cancelled", message: "The operation was cancelled.")
                         }
-                        else if self.scaleVideo?.outOfOrder == true {
+                        else if let scaleVideo = self.scaleVideo, scaleVideo.outOfOrder {
                             self.alertInfo = AlertInfo(id: .scalingFailed, title: "Scaling Failed", message: "Scaling produced out of order presentation times.\n\nTry different settings for factor, modifer or frame rate.")
                         }
                         else {
